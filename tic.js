4f24c512b18b7326dfe43692130b1e02c8bf5092
Kevin <kevin03.nguyen@gmail.com>
2020-07-23 21:28:46 -0400

    implemented all the winning conditions

--- tic.js
+++ tic.js
@@ -3,7 +3,8 @@ const gameBoard = (() => {
     let player = [];
 
 
-    const setBoard = (size) => {
+    const setBoard = (size = 3) => {
+        if(size < 3){size = 3};
         for(let i = 0; i < size; i++){
             
             let arr = [];
@@ -31,27 +32,55 @@ const gameBoard = (() => {
 
     const _winVert = () => {
 
+        
+        for (let i = 0; i < board.length; i++) {
+            let marker = board[0][i];    
+            for(let j = 0; j < board.length; j++){
+               if(board[j][i] !== marker){return false;}
+            }
+            
+        }
+        return true;
     };
 
     const _winHor = () => {
-
+        for(let i = 0; i < board.length; i++){
+            let marker = board[i][0];
+           
+            for(let j = 0; j < board.length; j++){
+                if(board[i][j] !== marker ){return false;}
+            }
+            
+        }
+        return true;
     };
 
-    const _winDiag = () => {
+    const _winDiagLeft = () => {
+        let marker = board[0][0];
+        for (let i = 0; i < array.length; i++) {
+            if(board[i][i] !== marker){return false;}
+            
+        }
+        return true;
+    };
 
+    const _winDiagRight = () => {
+        let marker = board[0][board.length - 1];
+        let j = board.length - 1;
+        for (let i = 0; i < array.length; i++) {
+            if(board[i][j] !== marker){
+                j--;
+                return false;}
+            
+        }
+        return true;
     };
 
     const win = () => {
 
-        
+        if(_winVert() || _winHor() || _winDiagLeft() || _winDiagRight()){return true;}
 
-        if(_winVert() || _winHor() || _winDiag()){
-            return true;
-        }
-
-        
-            return false;
-        
+        return false;
     }
 
     const _tie = () => {
